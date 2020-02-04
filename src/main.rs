7ef7a273b55f765a6b33b92df1d70b4246f82fd1
Koen Wilde <koen@chillheid.nl>
2020-02-04 13:42:04 +0100

    Apply `cargo fmt` suggestions

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use warp::Filter;
 async fn main() {
     let port: u16 = std::env::var("HELLO_PORT")
         .unwrap_or("8000".to_string())
-        .parse().expect("HELLO_PORT is set to an invalid port number");
+        .parse()
+        .expect("HELLO_PORT is set to an invalid port number");
 
     let routes = warp::any().map(|| "Hello, world!\n");
 
